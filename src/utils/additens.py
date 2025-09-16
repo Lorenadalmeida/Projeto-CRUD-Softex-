c3d9243536e7c00a34ce87bd9b2f1b3beda4f92b
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-16 23:19:08 +0000

    Teste de push.

--- src/utils/additens.py
+++ src/utils/additens.py
@@ -1,1 +1,17 @@
 #Lorena
+planetas = [] 
+def adicionar_planeta():
+    nome = input("digite o nome do planeta:" )
+    tamanho = float(input("digite o tamanho do planeta (em km):"))
+    distancia = float(input("digite a distancia do sol (em milhões de km):"))
+
+    planeta = {
+        "nome": nome,
+        "tamanho": tamanho,
+        "distancia": distancia 
+        }  
+    planetas.append(planeta)
+    print(f"\n planeta '{nome}) registrado com sucesso!")
+    print(planetas)
+
+adicionar_planeta()
